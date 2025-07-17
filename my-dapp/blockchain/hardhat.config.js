4f3687d471439894a5f5dafe9e9978610d1dbd43
QuangNM <minhquangnguyen1996@gmal.com>
2025-07-17 11:40:47 +0700

    add source deploy to Sepolia and Vercel

--- my-dapp/blockchain/hardhat.config.js
+++ my-dapp/blockchain/hardhat.config.js
@@ -1,10 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545"
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
     }
   }
 };
